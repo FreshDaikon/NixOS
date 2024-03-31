0159d6e77ca767a8383f38500bc266168ddd5f92
FreshDaikon <runerask86@gmail.com>
2024-04-01 00:24:40 +0200

    yuh

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -2,7 +2,8 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ 
+      # Include the results of the hardware scan.
       ./hardware-configuration.nix
       #inputs.home-manager.nixosModules.default
     ];
